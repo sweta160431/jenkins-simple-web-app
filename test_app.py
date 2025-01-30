161044bf3122811cd8c7ff28afa56db017fa8a7d
SwetaOnCloud <139581814+sweta160431@users.noreply.github.com>
2025-01-31 01:46:46 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,6 +3,5 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, Automate code testing and deployment using a
-Continuous Integration/Continuous Deployment (CI/CD) pipeline!"
+    assert response.data == b"Hello, Automate code testing and deployment using a Continuous Integration/Continuous Deployment (CI/CD) pipeline!"
 

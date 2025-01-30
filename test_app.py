efc20634cf3bd2f3abd434cfc26056ce1ce8586b
SwetaOnCloud <139581814+sweta160431@users.noreply.github.com>
2025-01-31 01:34:42 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,5 +3,6 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, CI/CD with Jenkins and Docker!"
+    assert response.data == b"Hello, Automate code testing and deployment using a
+Continuous Integration/Continuous Deployment (CI/CD) pipeline!"
 

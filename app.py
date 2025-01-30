b0658215748c1a0fc5844209203dee5220584e4b
SwetaOnCloud <139581814+sweta160431@users.noreply.github.com>
2025-01-31 01:43:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Automate code testing and deployment using a
-Continuous Integration/Continuous Deployment (CI/CD) pipeline..!"
+Continuous Integration/Continuous Deployment (CI/CD) pipeline!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
